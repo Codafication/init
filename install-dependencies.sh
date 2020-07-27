5b8d999bf511e79625cd7a60c00fd04fe63a8f2c
Liam Mackie <liam.mackie@codafication.com>
2020-07-28 09:33:56 +1000

    Fixed paths for dependencies scripts

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -12,9 +12,9 @@ esac
 echo "Machine=$machine"
 
 if [ "$machine" == "Linux" ]; then
-  ./scripts/install-dependencies-linux.sh
+  ./install-dependencies-linux.sh
 elif [ "$machine" == "Mac" ]; then
-  ./scripts/install-dependencies-osx.sh
+  ./install-dependencies-osx.sh
 else
     echo "Error: The machine does not appear to be Mac or Linux, found $machine"
     exit 1
